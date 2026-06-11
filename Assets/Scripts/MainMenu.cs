8fe86c9ab7e3dc6b4c6682c13a5609677b97c3e0
juangalmeida-afk <juan_g_almeida@estudante.sesisenai.org.br>
2026-06-11 17:10:55 -0300

    ultima atualização(eu acho kkk)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuPrincipal : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
+    public GameObject painelPausa;
+    public GameObject botaoPausa;
+
+    private bool pausado = false;
+
+    void Start()
+    {
+        if (painelPausa != null)
+            painelPausa.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausado)
+            {
+                ContinuarJogo();
+            }
+            else
+            {
+                AbrirMenuPausa();
+            }
+        }
+    }
+
+    // MENU PRINCIPAL
+
     public void Jogar()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(1);
     }
 
@@ -15,5 +46,39 @@ public class MenuPrincipal : MonoBehaviour
 #else
         Application.Quit();
 #endif
+
+        Debug.Log("Jogo fechado");
+    }
+
+    // MENU DE PAUSA
+
+    public void AbrirMenuPausa()
+    {
+        if (painelPausa != null)
+            painelPausa.SetActive(true);
+
+        if (botaoPausa != null)
+            botaoPausa.SetActive(false);
+
+        Time.timeScale = 0f;
+        pausado = true;
+    }
+
+    public void ContinuarJogo()
+    {
+        if (painelPausa != null)
+            painelPausa.SetActive(false);
+
+        if (botaoPausa != null)
+            botaoPausa.SetActive(true);
+
+        Time.timeScale = 1f;
+        pausado = false;
+    }
+
+    public void VoltarMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
     }
 }
